57d04d07154861d5678ccdab665958fe8c939c5f
staubichsauger <49568580+staubichsauger@users.noreply.github.com>
2020-03-21 17:54:09 +0100

    type mgmt

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/staubichsauger/FreezerGopher
 go 1.14
 
 require (
+	github.com/hako/durafmt v0.0.0-20191009132224-3f39dc1ed9f4 // indirect
 	github.com/jinzhu/gorm v1.9.12 // indirect
 	github.com/sirupsen/logrus v1.4.2 // indirect
 )
--- main.go
+++ main.go
@@ -22,7 +22,22 @@ type PerishableType struct {
 	gorm.Model
 	Name           string
 	IsFresh        bool
-	AdditionalTime time.Duration
+	AdditionalTime int
+	TimeUnit	   string
+}
+
+type PerishableTypeString struct {
+	Name           string
+	IsFresh        bool
+	AdditionalTime string
+	TimeUnit	   string
+}
+
+type PerishableTypePost struct {
+	Name           string
+	IsFresh        string
+	AdditionalTime string
+	TimeUnit       string
 }
 
 var (
@@ -49,52 +64,87 @@ func main() {
 	http.Handle("/vendor/", http.StripPrefix("/vendor/", vendor))
 
 	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/add", addHandler)
 	http.HandleFunc("/addType", addTypeHandler)
+	http.HandleFunc("/addTypePost", addTypePostHandler)
+	http.HandleFunc("/manageType", manageTypeHandler)
 
 	http.ListenAndServe(":8181", nil)
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	var perishableTypes []PerishableType
+	var out []PerishableTypeString
 	//db.Lock()
 	db.Find(&perishableTypes)
+	for _, pt := range perishableTypes {
+		out = append(out, PerishableTypeString{
+			Name:           pt.Name,
+			IsFresh:        pt.IsFresh,
+			AdditionalTime: strconv.Itoa(pt.AdditionalTime),
+			TimeUnit:       pt.TimeUnit,
+		})
+	}
 	//db.Unlock()
-
-	tpl.ExecuteTemplate(w, "index.gohtml", perishableTypes)
+	tpl.ExecuteTemplate(w, "index.gohtml", out)
 }
 
-func addHandler(w http.ResponseWriter, r *http.Request) {
-	tpl.ExecuteTemplate(w, "add.gohtml", nil)
+func manageTypeHandler(w http.ResponseWriter, r *http.Request) {
+	var perishableTypes []PerishableType
+	var out []PerishableTypeString
+	//db.Lock()
+	db.Find(&perishableTypes)
+	for _, pt := range perishableTypes {
+		out = append(out, PerishableTypeString{
+			Name:           pt.Name,
+			IsFresh:        pt.IsFresh,
+			AdditionalTime: strconv.Itoa(pt.AdditionalTime),
+			TimeUnit:       pt.TimeUnit,
+		})
+	}
+	//db.Unlock()
+	tpl.ExecuteTemplate(w, "manage.gohtml", out)
 }
 
 func addTypeHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	out := PerishableTypePost{
+		Name:           r.FormValue("name"),
+		IsFresh:        r.FormValue("isFresh"),
+		AdditionalTime: r.FormValue("addedTime"),
+		TimeUnit:       r.FormValue("timeUnit"),
+	}
+	tpl.ExecuteTemplate(w, "add.gohtml", out)
+}
+
+func addTypePostHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	isFresh, _ := strconv.ParseBool(r.FormValue("isFresh"))
 	addedTimeInt, err := strconv.Atoi(r.FormValue("addedTime"))
 	if err != nil {
 		logrus.Errorf("Error parsing addedTimeInt: %v", err)
 	}
-	var additionalTime time.Duration
-	switch r.FormValue("timeUnit") {
-	case "d":
-		additionalTime = time.Duration(addedTimeInt) * time.Hour * 24
-	case "w":
-		additionalTime = time.Duration(addedTimeInt) * time.Hour * 24 * 7
-	case "m":
-		additionalTime = time.Duration(addedTimeInt) * time.Hour * 24 * 30
-	}
 
-	t := PerishableType{
-		Model:          gorm.Model{},
-		Name:           r.FormValue("name"),
-		IsFresh:        isFresh,
-		AdditionalTime: additionalTime,
-	}
+	timeUnit := r.FormValue("timeUnit")
 
-	//db.Lock()
+	var t PerishableType
+	if  db.Where("name = ?", r.FormValue("name")).First(&t).RecordNotFound() {
+		t = PerishableType{
+			Model:          gorm.Model{},
+			Name:           r.FormValue("name"),
+			IsFresh:        isFresh,
+			AdditionalTime: addedTimeInt,
+			TimeUnit:	    timeUnit,
+		}
+	} else {
+		t.IsFresh = isFresh
+		t.AdditionalTime = addedTimeInt
+		t.TimeUnit = timeUnit
+	}
 	db.Save(&t)
-	//db.Unlock()
 
-	http.Redirect(w, r, "/add", http.StatusFound)
+	if r.FormValue("submit") == "add" {
+		http.Redirect(w, r, "/addType", http.StatusFound)
+		return
+	}
+	http.Redirect(w, r, "/manageType", http.StatusFound)
 }
